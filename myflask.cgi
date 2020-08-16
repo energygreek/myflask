52bb9cff4044de4e2dffed12222d0167f7c081dc
sui <289915889@qq.com>
2020-08-16 19:24:34 +0800

    add log to cgiserver

--- myflask.cgi
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from flup.server.fcgi import WSGIServer
-from src.myapp import app
-
-if __name__ == '__main__':
-    # for apache
-    # WSGIServer(app).run()
-    # for nginx, need to set conmunication sock between nginx and the cgiserver
-    WSGIServer(app, bindAddress='/var/sockets/myflask-fcgi.sock').run()
---
+++ myflask.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from flup.server.fcgi import WSGIServer
+from src.myapp import app
+import logging
+from logging.handlers import RotatingFileHandler
+
+ERROR_LOG_FILE='/var/log/myflask/myflask.log'
+# create a log file
+log = open(ERROR_LOG_FILE, 'w')
+log.seek(0)
+log.truncate()
+log.write("Web app log\n")
+log.close()
+
+log_handler=RotatingFileHandler(ERROR_LOG_FILE, maxBytes=1000000, backupCount=1)
+formatter = logging.Formatter(
+    "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
+    )
+log_handler.setFormatter(formatter)
+app.logger.setLevel(logging.DEBUG)
+app.logger.addHandler(log_handler)
+
+
+
+if __name__ == '__main__':
+    # for apache
+    # WSGIServer(app).run()
+    # for nginx, need to set conmunication sock between nginx and the cgiserver
+    WSGIServer(app, bindAddress='/var/run/myflask-fcgi.sock',log=app.logger).run()
